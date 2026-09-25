105a892d45e3f7ac3e3aa903ab01c0e8f1fe6284
Anton <kiryanov.a.f@edu.mirea.ru>
2026-09-25 11:09:10 +0300

    feat: add phone validation function

--- validator.py
+++ validator.py
@@ -1,4 +1,8 @@
-# validator.py
+def validate_phoness(phone: str) -> bool:
+    """Валидация российского номера телефона."""
+    import re
+    pattern = r'^\+?7\d{10}$'
+    return bool(re.match(pattern, phone.replace('-', '').replace(' ', '')))
 def validate_email(email: str) -> bool:
     """Валидация email-адреса."""
     import re
